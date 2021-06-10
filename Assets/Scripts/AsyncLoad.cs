9fa982778d17def6b480f124924866c38cac0519
Gerard <gerardarditcastells@enti.cat>
2021-06-10 20:08:47 +0200

    project init

---
+++ Assets/Scripts/AsyncLoad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AsyncLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AsyncOperation loadingOperation;
+    void Start()
+    {
+        Resources.UnloadUnusedAssets();
+        loadingOperation = SceneManager.LoadSceneAsync(FlowManager.Instance.nextSceneToLoad);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float loadProgress = loadingOperation.progress;
+        Debug.Log(loadProgress);
+    }
+
+
+}
---
+++ Assets/Scripts/FlowManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class FlowManager : MonoBehaviour
+{
+    private static FlowManager instance;
+    public static FlowManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private bool created;
+    public string nextSceneToLoad = "InGame";
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("FlowManager");
+
+        if (objs.Length > 1 && !created)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            created = true;
+            DontDestroyOnLoad(gameObject);
+
+            instance = this;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GoToMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void GoToInit() { }
+    public void GoToMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+    public void GoToInGame() { }
+}
